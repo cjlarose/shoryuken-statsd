1feea0e25d02b37c35b19eef2cc2403aac9fc9d6
Chris LaRose <cjlarose@gmail.com>
2021-07-12 23:34:28 -0700

    Leverage new :utilization_update event

--- lib/shoryuken/statsd/listener.rb
+++ lib/shoryuken/statsd/listener.rb
@@ -8,22 +8,15 @@ module Shoryuken
       end
 
       def register(shoryuken_config)
-        shoryuken_config.on :dispatch, &method(:handle_dispatch)
+        shoryuken_config.on :utilization_update, &method(:handle_utilization_update)
       end
 
       private
 
-      def handle_dispatch(event)
-        puts "dispatch: #{event}"
-        report_processor_metrics group: event[:group],
-                                 manager: event[:manager]
-        # report initial stats on first dispatch
-        # After first dispatch, just update the dispatch counter
-      end
-
-      def report_processor_metrics(group:, manager:)
-        @reporter.report_busy_processors group, manager.send(:busy)
-        @reporter.report_max_processors group, manager.instance_variable_get(:@max_processors)
+      def handle_utilization_update(event)
+        group = event[:group]
+        @reporter.report_max_processors group, event[:max_processors]
+        @reporter.report_busy_processors group, event[:busy_processors]
       end
     end
   end
